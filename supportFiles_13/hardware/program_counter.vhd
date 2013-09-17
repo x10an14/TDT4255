edb8e7730d365e2519a67b3741634751065725d3
Hanna Holler Kamperud <hannahollerkamperud@iMac.local>
2013-09-17 23:37:47 +0200

    Added the files I've started working on. The PC is 'done' until I figure out if I've done it right, and the control unit has implemented R-instructions and just started the branch-instruction. So I'm still working on the control unit.

---
+++ supportFiles_13/hardware/program_counter.vhd
@@ -0,0 +1,35 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+library WORK;
+use WORK.MIPS_CONSTANT_PKG.ALL;
+
+
+entity program_counter is
+	port(
+			CLK 	: in 	STD_LOGIC;
+			RESET	: in 	STD_LOGIC;
+			PC_CON	: in 	STD_LOGIC;
+			PC_IN	: in 	STD_LOGIC_VECTOR (IADDR_BUS-1 downto 0);
+			PC_OUT	: out 	STD_LOGIC_VECTOR (IADDR_BUS-1 downto 0)
+	);
+end program_counter;
+
+architecture Behavioral of program_counter is
+	
+	signal REG : STD_LOGIC_VECTOR (IADDR_BUS-1 downto 0);
+
+begin
+
+	UPDATE_PC: process(PC_CON)
+	begin
+		--some reset code...
+		if rising_edge(PC_CON) then
+			REG <= PC_IN;
+		
+	PC_OUT <= REG;
+
+end Behavioral
+
+
