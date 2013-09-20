a05c64f56dc2ee2161d758a87863ba0c7d9205cf
blackCheetah <idefix@dhcp-017021.wlan.ntnu.no>
2013-09-20 13:59:03 +0200

    I have finished the control unit. Will synthesize in ISE now, so it's probably not bug free yet

--- supportFiles_13/hardware/control_unit.vhd
+++ supportFiles_13/hardware/control_unit.vhd
@@ -27,14 +27,12 @@ end control_unit;
 
 architecture Behavioral of control_unit is
 
-	signal state : STD_LOGIC_VECTOR(2 downto 0);
+	type state is (ALU_FETCH, ALU_EXE, READ_STALL, WRITE_STALL)
 
 begin
 
 	ALU_STATE_MACHINE: process(CLK, RESET, OpCode)
-		constant ALU_FETCH 	: STD_LOGIC_VECTOR(1 downto 0) := "00";
-		constant ALU_EXE	: STD_LOGIC_VECTOR(1 downto 0) := "01";
-		constant ALU_STALL 	: STD_LOGIC_VECTOR(1 downto 0) := "10";
+		
 	begin
 		if rising_edge(CLK) then
 			if reset = '1' then
@@ -95,6 +93,86 @@ begin
 								SRWriteEnb	<= '1';	--setting the zero flag if equal
 								
 								state 		<= ALU_FETCH;
+
+							when "100011" =>	--Load word
+								RegDst		<= '0';
+								Branch		<= '0';
+								MemRead		<= '1';
+								MemtoReg	<= '1';
+
+								ALUOp.Op0	<= '0';
+								ALUOp.Op1	<= '0';
+								ALUOp.Op2	<= '0';
+
+								MemWrite	<= '0';
+								ALUSrc		<= '1';
+								RegWrite	<= '0';	--Have to wait until data has been updated
+								Jump		<= '0';
+								PCWriteEnb	<= '0';
+								SRWriteEnb	<= '0';	
+
+								state 		<= READ_STALL;
+							when "101011" =>	--Store word
+								RegDst		<= '0';
+								Branch		<= '0';
+								MemRead		<= '0';
+								MemtoReg	<= '0';
+
+								ALUOp.Op0	<= '0';
+								ALUOp.Op1	<= '0';
+								ALUOp.Op2	<= '0';
+
+								MemWrite	<= '1';
+								ALUSrc		<= '1';
+								RegWrite	<= '0';
+								Jump		<= '0';
+								PCWriteEnb	<= '0';
+								SRWriteEnb	<= '0';	
+
+								state 		<= WRITE_STALL;
+							when "001000" =>	--Load immidiate. (Implemented as add immidiate where you add with the zero register)
+								RegDst		<= '0';
+								Branch		<= '0';
+								MemRead		<= '0';
+								MemtoReg	<= '0';
+
+								ALUOp.Op0	<= '1';
+								ALUOp.Op1	<= '1';
+								ALUOp.Op2	<= '0';
+
+								MemWrite	<= '0';
+								ALUSrc		<= '1';
+								RegWrite	<= '1';
+								Jump		<= '0';
+								PCWriteEnb	<= '0';
+								SRWriteEnb	<= '0';	
+
+								state 		<= ALU_FETCH;
+							when "000010" =>	--Jump
+								RegDst		<= '0';
+								Branch		<= '0';
+								MemRead		<= '0';
+								MemtoReg	<= '0';
+
+								MemWrite	<= '0';
+								ALUSrc		<= '0';
+								RegWrite	<= '0';
+								Jump		<= '1';
+								PCWriteEnb	<= '0';
+								SRWriteEnb	<= '0';	
+
+								state 		<= ALU_FETCH;
+						end case;
+					when READ_STALL =>
+						RegWrite 	<= '1';
+						state 		<= ALU_FETCH;
+					when WRITE_STALL =>
+						state 		<= ALU_FETCH;
+				end case;
+			end if;
+		end if;
+	end process;
+end Behavioral;
 
 
 
