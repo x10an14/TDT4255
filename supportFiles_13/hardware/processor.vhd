560bca6228d2b2401e50773526dc1f14c65d5c05
x10an14 <x10an14@hotmail.com>
2013-09-27 10:49:13 +0200

    Added a commented 'component list' into processor.vhd

--- supportFiles_13/hardware/processor.vhd
+++ supportFiles_13/hardware/processor.vhd
@@ -17,12 +17,19 @@ entity processor is
 		dmem_address_wr		: out STD_LOGIC_VECTOR (DADDR_BUS - 1 downto 0);
 		dmem_data_out			: out STD_LOGIC_VECTOR (DDATA_BUS - 1 downto 0);
 		dmem_write_enable		: out STD_LOGIC
-	);		
+	);
 end processor;
 
 architecture behave of processor is
 	--component ALU_control
-
+	--component ALU
+	--component PC
+	--component sign extender
+	--component instruction memory
+	--component register memory
+	--component data memory
+	--component PC adder
+	--component ALU jump adder
 begin
 	--blabla
 end behave;
