bb54ab2e35a2a15ec3b318eedec2218074d67e66
x10an14 <x10an14@hotmail.com>
2013-09-29 14:11:08 +0200

    Commented out added lines

--- supportFiles_13/hardware/supplied/mips_constant_pkg.vhd
+++ supportFiles_13/hardware/supplied/mips_constant_pkg.vhd
@@ -1,7 +1,7 @@
 --	Package File Template
 --
---	Purpose: This package defines supplemental types, subtypes, 
---		 constants, and functions 
+--	Purpose: This package defines supplemental types, subtypes,
+--		 constants, and functions
 
 
 library IEEE;
@@ -16,7 +16,7 @@ package MIPS_CONSTANT_PKG is
 		Op1	:	STD_LOGIC;
 		Op2	:	STD_LOGIC;
 	end record;
-	
+
 	type ALU_INPUT is
 	record
 		Op0		:	STD_LOGIC;
@@ -32,7 +32,7 @@ package MIPS_CONSTANT_PKG is
 		Zero		: STD_LOGIC;
 		Negative	: STD_LOGIC;
 	end record;
-	
+
   -- NEW!
 	type BRANCH_TYPE is (COND_BRANCH, JUMP, NO_BRANCH);
   type ALU_OP      is (ALUOP_LOAD_STORE, ALUOP_BRANCH, ALUOP_FUNC, ALUOP_LDI);
@@ -43,23 +43,23 @@ package MIPS_CONSTANT_PKG is
 	constant DADDR_BUS	: integer	:= 32;
 	constant DDATA_BUS	: integer	:= 32;
 	constant RADDR_BUS	: integer	:= 5;
-	
+
 	constant MEM_ADDR_COUNT	: integer	:= 8;
-	
+
 	constant ZERO1b	: STD_LOGIC							  :=  '0';
-	constant ZERO32b	: STD_LOGIC_VECTOR(31 downto 0) :=  "00000000000000000000000000000000";	
+	constant ZERO32b	: STD_LOGIC_VECTOR(31 downto 0) :=  "00000000000000000000000000000000";
 	constant ZERO16b	: STD_LOGIC_VECTOR(15 downto 0) :=  "0000000000000000";
-	constant ONE32b	: STD_LOGIC_VECTOR(31 downto 0) :=  "11111111111111111111111111111111";	
-	constant ONE16b	: STD_LOGIC_VECTOR(15 downto 0) :=  "1111111111111111";	
-	
-	constant ALU_OP_AND	: ALU_INPUT := (others => '0');								-- AKA X"0"
-	constant ALU_OP_OR	: ALU_INPUT := (Op0 = '1', others => '0');				-- AKA X"1"
-	constant ALU_OP_ADD	: ALU_INPUT := (Op1 = '1', others => '0');				-- AKA X"2"
-	constant ALU_OP_SLT	: ALU_INPUT := (Op3 = '0', others => '1');				-- AKA X"7"
-	constant ALU_OP_SUB	: ALU_INPUT := (Op2 = '1', Op1 = '1', others => '0');	-- AKA X"6"
-	
-	constant ALU_OP_ZERO	: ALU_OP_INPUT := (others => '0');
-	constant ALU_OP_ONE	: ALU_OP_INPUT	:= (Op0 = '1', others => '0');
-	constant ALU_OP_TWO	: ALU_OP_INPUT := (Op1 = '1', others => '0');
-	
+	constant ONE32b	: STD_LOGIC_VECTOR(31 downto 0) :=  "11111111111111111111111111111111";
+	constant ONE16b	: STD_LOGIC_VECTOR(15 downto 0) :=  "1111111111111111";
+
+	-- constant ALU_OP_AND	: ALU_INPUT := (others => '0');								-- AKA X"0"
+	-- constant ALU_OP_OR	: ALU_INPUT := (Op0 = '1', others => '0');				-- AKA X"1"
+	-- constant ALU_OP_ADD	: ALU_INPUT := (Op1 = '1', others => '0');				-- AKA X"2"
+	-- constant ALU_OP_SLT	: ALU_INPUT := (Op3 = '0', others => '1');				-- AKA X"7"
+	-- constant ALU_OP_SUB	: ALU_INPUT := (Op2 = '1', Op1 = '1', others => '0');	-- AKA X"6"
+
+	-- constant ALU_OP_ZERO	: ALU_OP_INPUT := (others => '0');
+	-- constant ALU_OP_ONE	: ALU_OP_INPUT	:= (Op0 = '1', others => '0');
+	-- constant ALU_OP_TWO	: ALU_OP_INPUT := (Op1 = '1', others => '0');
+
 end MIPS_CONSTANT_PKG;
