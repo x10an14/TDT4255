c5c99486f716b44b33ff4cc2c8da64bf36c26d69
x10an14 <x10an14@hotmail.com>
2013-10-02 21:53:29 +0200

    First 're-written' instantiation of program counter

--- supportFiles_13/hardware/program_counter.vhd
+++ supportFiles_13/hardware/program_counter.vhd
@@ -10,28 +10,28 @@ entity program_counter is
 	port(
 			CLK 		: in 	STD_LOGIC;
 			RESET		: in 	STD_LOGIC;
-			PC_CON	: in 	STD_LOGIC;
+			PC_WR_EN	: in 	STD_LOGIC; --ProgramCounter Write Enable pin
 			PC_IN		: in 	STD_LOGIC_VECTOR (IADDR_BUS-1 downto 0);
 			PC_OUT	: out STD_LOGIC_VECTOR (IADDR_BUS-1 downto 0)
 	);
 end program_counter;
 
 architecture Behavioral of program_counter is
-	
 	signal REG : STD_LOGIC_VECTOR (IADDR_BUS-1 downto 0);
 
 begin
-
-	UPDATE_PC: process(PC_CON) is
+	process(CLK) is
 	begin
-		--some reset code...
-		if rising_edge(PC_CON) then
-			REG <= PC_IN;
-		end if;
-		
-	PC_OUT <= REG;
+		if rising_edge(CLK) then
+			-- Here's were we decide what input (if any) to utilize
+			if  PC_WR_EN = '1' then
+				REG <= PC_IN;
+			elsif RESET = '1' then
+				REG <= X"00000"; --Hardcoded reset value
+			end if;
+
+			-- Here's where we set output
+			PC_OUT <= REG;
+
 	end process;
-
 end Behavioral;
-
-
