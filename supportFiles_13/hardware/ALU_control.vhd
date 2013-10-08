51ec6a27110a4f0f4c9f0406bc42159921fb3bf3
x10an14 <x10an14@hotmail.com>
2013-10-08 19:20:47 +0200

    OCD whitespace formatting

--- supportFiles_13/hardware/ALU_control.vhd
+++ supportFiles_13/hardware/ALU_control.vhd
@@ -16,31 +16,31 @@ entity ALU_control is
 end ALU_control;
 
 architecture Behavioral of ALU_control is
-	
+
 begin
 	ALU_LOGIC: process(ALUOp, FUNC)
 	begin
-      if FUNC = "000000" then -- R instructions
-        if (aluop.Op1 = '0' and aluop.Op2 = '0' and aluop.Op0 = '0') then --add 
-            ALU_FUNC.Op0 <= '0';
-            ALU_FUNC.Op1 <= '1';
-            ALU_FUNC.Op2 <= '0';
-            ALU_FUNC.Op3 <= '0';
-        else
-                  --something else
-        end if;
-      elsif FUNC = "000100" then -- BEQ ins, act as subtracter
-            ALU_FUNC.Op0 <= '0';
-            ALU_FUNC.Op1 <= '1';
-            ALU_FUNC.Op2 <= '1';
-            ALU_FUNC.Op3 <= '0';
-      else  -- in all other cases act as adder
-            ALU_FUNC.Op0 <= '0';
-            ALU_FUNC.Op1 <= '1';
-            ALU_FUNC.Op2 <= '0';
-            ALU_FUNC.Op3 <= '0';      
-      end if;
-   
+			if FUNC = "000000" then -- R instructions
+				if (aluop.Op1 = '0' and aluop.Op2 = '0' and aluop.Op0 = '0') then --add
+						ALU_FUNC.Op0 <= '0';
+						ALU_FUNC.Op1 <= '1';
+						ALU_FUNC.Op2 <= '0';
+						ALU_FUNC.Op3 <= '0';
+				else
+									--something else
+				end if;
+			elsif FUNC = "000100" then -- BEQ ins, act as subtracter
+						ALU_FUNC.Op0 <= '0';
+						ALU_FUNC.Op1 <= '1';
+						ALU_FUNC.Op2 <= '1';
+						ALU_FUNC.Op3 <= '0';
+			else	-- in all other cases act as adder
+						ALU_FUNC.Op0 <= '0';
+						ALU_FUNC.Op1 <= '1';
+						ALU_FUNC.Op2 <= '0';
+						ALU_FUNC.Op3 <= '0';
+			end if;
+
 
 	end process;
-end Behavioral; 
+end Behavioral;
