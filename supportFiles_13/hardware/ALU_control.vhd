59f0a35cdafcf438d2d943f32199a5e95e5d9b12
x10an14 <x10an14@gmail.com>
2013-10-09 04:15:42 +0200

    Removed all synthesized latches from alu_control.vhd

--- supportFiles_13/hardware/ALU_control.vhd
+++ supportFiles_13/hardware/ALU_control.vhd
@@ -1,4 +1,4 @@
- library IEEE;
+library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
@@ -16,51 +16,40 @@ entity ALU_control is
 end ALU_control;
 
 architecture Behavioral of ALU_control is
-
+	 constant R_Instr		: ALU_OP_INPUT := (others => '0');
+	 constant BEQ_Instr	: ALU_OP_INPUT := (Op0 => '0', others => '1');
+	 constant LwSw_Instr	: ALU_OP_INPUT := (Op0 => '1', others => '0');
+	 
 begin
 	ALU_LOGIC: process(ALUOp, FUNC)
 	begin
-			if (aluop.Op1 = '0' and aluop.Op2 = '0' and aluop.Op0 = '0') then -- r instructions
-						case FUNC is
-							when "100000" => --add OpCode
-								ALU_FUNC.Op0 <= '0';
-								ALU_FUNC.Op1 <= '1';
-								ALU_FUNC.Op2 <= '0';
-								ALU_FUNC.Op3 <= '0';
-							when "100010" =>	--subtract OpCode
-								ALU_FUNC.Op0 <= '0';
-								ALU_FUNC.Op1 <= '1';
-								ALU_FUNC.Op2 <= '1';
-								ALU_FUNC.Op3 <= '0';
-							when "101010" =>	--slt OpCode
-								ALU_FUNC.Op0 <= '1';
-								ALU_FUNC.Op1 <= '1';
-								ALU_FUNC.Op2 <= '1';
-								ALU_FUNC.Op3 <= '0';
-							when "100100" =>	--and OpCode
-								ALU_FUNC.Op0 <= '0';
-								ALU_FUNC.Op1 <= '0';
-								ALU_FUNC.Op2 <= '0';
-								ALU_FUNC.Op3 <= '0';
-							when "100101" =>	--or OpCode
-								ALU_FUNC.Op0 <= '1';
-								ALU_FUNC.Op1 <= '0';
-								ALU_FUNC.Op2 <= '0';
-								ALU_FUNC.Op3 <= '0';
-							when others =>
-						end case;
-			elsif (aluop.Op0 = '0' and aluop.Op1 = '1' and aluop.Op2 = '1') then -- beq, do subtract function
-						ALU_FUNC.Op0 <= '0';
-						ALU_FUNC.Op1 <= '1';
-						ALU_FUNC.Op2 <= '1';
-						ALU_FUNC.Op3 <= '0';
-			elsif (aluop.Op0 = '1' and aluop.Op1 = '0' and aluop.Op2 = '0') then -- ls, sw, load imm, do add function
-						ALU_FUNC.Op0 <= '0';
-						ALU_FUNC.Op1 <= '1';
-						ALU_FUNC.Op2 <= '0';
-						ALU_FUNC.Op3 <= '0';
-			else
-			end if;
+		ALU_FUNC.Op0 <= '0';
+		ALU_FUNC.Op1 <= '0';
+		ALU_FUNC.Op2 <= '0';
+		ALU_FUNC.Op3 <= '0';
+		if (ALUOp = R_Instr) then -- r instructions
+			case FUNC is
+				when "100000" => --add OpCode
+					ALU_FUNC.Op1 <= '1';
+				when "100010" =>	--subtract OpCode
+					ALU_FUNC.Op1 <= '1';
+					ALU_FUNC.Op2 <= '1';
+				when "101010" =>	--slt OpCode
+					ALU_FUNC.Op0 <= '1';
+					ALU_FUNC.Op1 <= '1';
+					ALU_FUNC.Op2 <= '1';
+				when "100100" =>	--and OpCode
+				when "100101" =>	--or OpCode
+					ALU_FUNC.Op0 <= '1';
+				when others =>
+			end case;
+		elsif (ALUOp = BEQ_Instr) then -- beq, do subtract function
+			ALU_FUNC.Op1 <= '1';
+			ALU_FUNC.Op2 <= '1';
+		elsif (ALUOp = LwSw_Instr) then -- ls, sw, load imm, do add function
+			ALU_FUNC.Op1 <= '1';
+		else
+		end if;
 	end process;
 
 end Behavioral;
